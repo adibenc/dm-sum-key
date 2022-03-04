7e9439e1dbc44d9729b5fccb8eac6059f864f212
adib-enc <adib35785@gmail.com>
2022-03-04 10:52:21 +0700

    init summarize

---
+++ sum.py
@@ -0,0 +1,15 @@
+
+def log(text):
+    def test(text):
+        print("[log]-",text)
+    
+    return test
+
+@log
+def summarize(text1):
+    summarizd = "1"
+    print("why")
+
+    # return summarizd
+
+summarize("sum test", text1="1")
